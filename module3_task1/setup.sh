c9a9fec17e76a1394ec5a40e5a90159971c25354
FatjonMileti <tonimileti@yahoo.com>
2023-01-06 04:51:22 -0800

    edit Makefile & setup.sh

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-apt-get update && apt-get install -y make build-essential wget && wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb && dpkg -i hugo_extended_0.109.0_linux-amd64.deb
-make build
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+sudo apt install ./hugo.deb
+rm hugo.deb
